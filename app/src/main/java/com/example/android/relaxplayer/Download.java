cd96f66e339a6cf41ede118590387d75b31311ba
Gergő Siska <cadmonadam@gmail.com>
2017-05-22 22:17:28 +0200

    modification and correction

--- app/src/main/java/com/example/android/relaxplayer/Download.java
+++ app/src/main/java/com/example/android/relaxplayer/Download.java
@@ -1,7 +1,11 @@
 package com.example.android.relaxplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by Adam Cadmon on 2017. 05. 15..
@@ -13,5 +17,51 @@ public class Download extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.download);
+    }
+
+    //Inflating the menu bar on the current screen.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //Creating appropriate intents for the menu items.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handling item selection
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_birds:
+                intent = new Intent(this, Birds.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_storm:
+                intent = new Intent(this, Storm.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_nocturnal:
+                intent = new Intent(this, Nocturnal.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_library:
+                intent = new Intent(this, Mylibrary.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_now_playing:
+                intent = new Intent(this, Nowplaying.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_download:
+                intent = new Intent(this, Download.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/android/relaxplayer/Storm.java
+++ app/src/main/java/com/example/android/relaxplayer/Storm.java
@@ -3,6 +3,9 @@ package com.example.android.relaxplayer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -41,6 +44,53 @@ public class Storm extends AppCompatActivity implements View.OnClickListener {
                 break;
             default:
                 break;
+        }
+    }
+
+    //Inflating the menu bar on the current screen.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    //Creating appropriate intents for the menu items.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handling item selection
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_birds:
+                intent = new Intent(this, Birds.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_storm:
+                intent = new Intent(this, Storm.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_nocturnal:
+                intent = new Intent(this, Nocturnal.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_library:
+                intent = new Intent(this, Mylibrary.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_now_playing:
+                intent = new Intent(this, Nowplaying.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_download:
+                intent = new Intent(this, Download.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
